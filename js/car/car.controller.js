fa0167a08ec1201c93f3f32f9b83f431a477d7c7
karelin-alexander <karelin.sasha@gmail.com>
2016-02-09 11:58:13 +0300

    new

--- js/car/car.controller.js
+++ js/car/car.controller.js
@@ -1,9 +1,10 @@
 (function(){
     'use strict';
-    angular.module('CarController', []).controller('carCtrl', function($scope,car){
-        var promiseObj = car.getData();
-        promiseObj.then(function(value){
-            $scope.carData = value;
+    angular.module('CarController', [])
+        .controller('carCtrl', function($scope,car){
+            var promiseObj = car.getData();
+                promiseObj.then(function(value){
+                 $scope.carData = value;
+            });
         });
-    });
 })();
